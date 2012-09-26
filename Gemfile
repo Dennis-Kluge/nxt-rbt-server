9cac8c9c6c7f1e56bca5e2263da8dbd7ca46617f
Horst Mumpitz <denniskluge@me.com>
2012-09-26 22:38:21 +0100

    Revert "added rvmrc for dependency management"
    This reverts commit a44b83a4b8230d99cc23dc19e8047237a6af16d8.

--- Gemfile
+++
@@ -1,6 +0,0 @@
-source :rubygems
-
-gem 'sinatra'
-gem 'thin'
-
-gem 'mongoid', '~> 3.0.0'
--- lib/waypoint.rb
+++
@@ -1,9 +0,0 @@
-class Waypoint
-	include Mongoid::Document
-	include Mongoid::Timestamps
-
-	field :x, type: Float
-	field :y, type: Float
-	field :feature, type: Symbol 	
-	
-end
